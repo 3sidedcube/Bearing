fd38cd0818b33422bed28069fe1f3cedc6ccd02e
Tim Matthews <tim@3sidedcube.com>
2015-07-08 13:57:56 +0100

    Move remote geocoding code to common base class. Also fix bug whereby only the first returned result is used, possibly multiple times.

--- src/main/java/net/atomcode/bearing/geocoding/GeocodingTask.java
+++ src/main/java/net/atomcode/bearing/geocoding/GeocodingTask.java
@@ -4,7 +4,23 @@ import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Log;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -104,5 +120,204 @@ public abstract class GeocodingTask<T> extends AsyncTask<T, Void, List<Address>>
 	protected boolean deviceHasNativeGeocoding()
 	{
 		return Geocoder.isPresent();
+	}
+
+	protected List<Address> addressForRemoteGeocodedQuery(HttpGet request)
+	{
+		StringBuilder data = new StringBuilder();
+		try
+		{
+			HttpClient client = new DefaultHttpClient();
+			HttpResponse response;
+
+			if (!isCancelled())
+			{
+				try
+				{
+					response = client.execute(request);
+				}
+				catch (ClientProtocolException ex)
+				{
+					ex.printStackTrace();
+					return null;
+				}
+
+				InputStream content = response.getEntity().getContent();
+
+				InputStreamReader inputStreamReader = new InputStreamReader(content);
+				BufferedReader reader = new BufferedReader(inputStreamReader);
+
+				String line;
+				while ((line = reader.readLine()) != null && !isCancelled())
+				{
+					data.append(line);
+				}
+			}
+		}
+		catch (IOException ex)
+		{
+			Log.e("Bearing", "Network error connecting to Google Geocoding API" + ex.getMessage());
+			return null;
+		}
+
+		try
+		{
+			/* The JSON response structure
+			{
+				"results": [
+					{
+						"formatted_address": <formatted_address>,
+						"geometry": {
+							"location": {
+								"lat": <latitude>
+								"lng": <longitude>
+							}
+						}
+					}
+				]
+			}
+			*/
+
+			if (!isCancelled())
+			{
+				JSONObject geocodeData = new JSONObject(data.toString());
+				JSONArray addresses = geocodeData.getJSONArray("results");
+
+				int resultsToRead = Math.min(resultCount, addresses.length());
+
+				List<Address> addressList = new ArrayList<Address>(resultsToRead);
+				for (int i = 0; i < resultsToRead; i++)
+				{
+					JSONObject result = addresses.getJSONObject(i);
+
+					JSONObject geometry = result.getJSONObject("geometry");
+					JSONObject locationData = geometry.getJSONObject("location");
+
+					Address address = new Address(locale);
+					address.setLatitude(locationData.getDouble("lat"));
+					address.setLongitude(locationData.getDouble("lng"));
+
+					String[] addressParts = TextUtils.split(result.getString("formatted_address"), ", ");
+					int addressLine = 0;
+					for (int addressPartIndex = 0; addressPartIndex < addressParts.length; ++addressPartIndex)
+					{
+						String addressPart = addressParts[addressPartIndex].trim();
+						// If the address part is just a number then prepend it to the next address part
+						if (TextUtils.isDigitsOnly(addressPart) && addressPartIndex < addressParts.length - 1)
+						{
+							addressParts[addressPartIndex + 1] = addressPart + ", " + addressParts[addressPartIndex + 1];
+						}
+						// Don't allow consecutive duplicate address lines
+						else if (addressLine == 0 || !addressPart.equals(address.getAddressLine(addressLine - 1)))
+						{
+							address.setAddressLine(addressLine, addressPart);
+							++addressLine;
+						}
+					}
+
+					JSONArray addressComponents = result.getJSONArray("address_components");
+					for (int componentIndex = 0; componentIndex < addressComponents.length(); componentIndex++)
+					{
+						JSONObject component = addressComponents.getJSONObject(componentIndex);
+						populateAddressFromGoogleMapComponent(address, component);
+					}
+
+					addressList.add(address);
+				}
+
+				return addressList;
+			}
+		}
+		catch (JSONException ex)
+		{
+			Log.e("Bearing", "Google Geocoding API format parsing failed! " + ex.getMessage());
+		}
+
+		return null;
+	}
+
+	private void populateAddressFromGoogleMapComponent(Address address, JSONObject component) throws JSONException
+	{
+		String componentLongName = component.getString("long_name");
+		String componentShortName = component.getString("long_name");
+		JSONArray componentTypes = component.getJSONArray("types");
+		for (int typeIndex = 0; typeIndex < componentTypes.length(); typeIndex++)
+		{
+			String typeText = componentTypes.getString(typeIndex);
+			switch (typeText)
+			{
+				case "country":
+				{
+					address.setCountryName(componentLongName);
+					address.setCountryCode(componentShortName);
+					break;
+				}
+				case "route":
+				{
+					address.setThoroughfare(componentLongName);
+					break;
+				}
+				case "administrative_area_level_1":
+				{
+					address.setAdminArea(componentLongName);
+					break;
+				}
+				case "administrative_area_level_2":
+				{
+					address.setSubAdminArea(componentLongName);
+					break;
+				}
+				case "locality":
+				case "ward":
+				{
+					address.setLocality(componentLongName);
+					break;
+				}
+				case "sublocality":
+				{
+					address.setSubLocality(componentLongName);
+					break;
+				}
+				case "neighborhood":
+				{
+					if (address.getSubLocality() == null) address.setSubLocality(componentLongName);
+					break;
+				}
+				case "premise":
+				{
+					address.setPremises(componentLongName);
+					break;
+				}
+				case "street_number":
+				{
+					if (address.getPremises() == null) address.setPremises(componentLongName);
+					break;
+				}
+				case "postal_code":
+				{
+					address.setPostalCode(componentLongName);
+					break;
+				}
+				case "natural_feature":
+				case "airport":
+				case "park":
+				case "point_of_interest":
+				case "intersection":
+				{
+					address.setFeatureName(componentLongName);
+					break;
+				}
+				case "administrative_area_level_3":
+				case "administrative_area_level_4":
+				case "administrative_area_level_5":
+				case "subpremise":
+				case "political":
+				case "colloquial_area":
+				default:
+				{
+					break;
+				}
+			}
+		}
 	}
 }
